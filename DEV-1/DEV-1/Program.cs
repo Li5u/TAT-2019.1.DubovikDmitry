0163d59d5ea22237668431ef30a5bde0e8a9f41b
Dmitry Dubovik <qwa.kusha99@gmail.com>
2019-03-07 01:17:56 +0300

    DEV-1 v.1.0

---
+++ DEV-1/DEV-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace DEV_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Program takes arguments from command line and displays to console
+        /// all substrings without consecutive repetitive symbols
+        /// </summary>
+        /// <param name="args">arguments from command line</param>
+        static void Main(string[] args)
+        {
+            SubstringSearcher searcher = new SubstringSearcher();
+            string main_string = args[0];
+            foreach (string substring in searcher.Find(main_string))
+            {
+                Console.WriteLine(substring);
+            }
+
+        }
+    }
+}
---
+++ DEV-1/DEV-1/SubstringSearher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DEV_1
+{   /// <summary>
+    /// Class can find substrings without consecutive repetitive symbols
+    /// </summary>
+    public class SubstringSearcher
+    {
+        /// <summary>
+        /// returns HashSet of substrings
+        /// </summary>
+        public HashSet<string> Find(string main_string)
+        {
+            HashSet<string> substrings = new HashSet<string>();
+            if (main_string.Length < 2)
+            {
+                Console.WriteLine("Wrong input!");
+                return substrings;
+            }
+            else
+            {
+                for (int i = 0; i < main_string.Length; i++)
+                {
+                    string current_substring = main_string[i].ToString();
+                    for (int j = i + 1; j < main_string.Length; j++)
+                    {
+                        if (main_string[j - 1] != main_string[j])
+                        {
+                            current_substring += main_string[j];
+                            substrings.Add(current_substring);
+                            //HashSet won't add current_substring if it also exists
+                            continue;
+                        }
+                        else
+                            break;
+                    }
+                }
+                return substrings;
+            }
+        }
+    }
+}
