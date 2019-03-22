f7c4af069b2a419407147cdfd896cb0c217e7eb5
Dmitry Dubovik <qwa.kusha99@gmail.com>
2019-03-22 13:17:09 +0300

    change EntryPoint

--- DEV-3/DEV-3/TeamSearcher.cs
+++ DEV-3/DEV-3/TeamSearcher.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DEV_3
 {
     /// <summary>
-    /// Abstract class for searchers by criteria.
+    /// Abstract class for searchers by criterion.
     /// </summary>
     abstract class TeamSearcher
     {
