dfaf699e49724872796a69d13a2ae87cbb1d325d
Dmitry Dubovik <qwa.kusha99@gmail.com>
2019-05-10 16:04:54 +0300

    main fixed

---
+++ DEV-9/DEV-9/Entrypoint.cs
@@ -0,0 +1,16 @@
+﻿namespace DEV_9
+{
+    /// <summary>
+    /// Main class of the program.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// Main method of the program.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
